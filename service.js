da1624f50c47f9691a47f1a70d58bd5c086be5dd
Quentin Grégoire <quentin.gregoire@businessdecision.be>
2019-06-04 10:23:26 +0200

    Add cache

--- service.js
+++ service.js
@@ -45,7 +45,31 @@ if (workbox) {
     }
   ]);
 
-  
+  workbox.routing.registerRoute(
+      /(.*)\.(?:png|gif|jpg|css)$/,
+      workbox.strategies.cacheFirst({
+        cacheName: 'design-cache',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          })
+        ]
+      })
+  );
+
+  workbox.routing.registerRoute(
+      "https://api.irail.be/stations/?format=json",
+      workbox.strategies.networkFirst({
+        cacheName: 'api-cache',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          })
+        ]
+      })
+  );
 
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
