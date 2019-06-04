59a878cc357e09c2df2526237ec18a9aab178508
Quentin Grégoire <quentin.gregoire@businessdecision.be>
2019-06-04 10:42:09 +0200

    Cache

--- service.js
+++ service.js
@@ -64,7 +64,6 @@ if (workbox) {
         cacheName: 'api-cache',
         plugins: [
           new workbox.expiration.Plugin({
-            maxEntries: 50,
             maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
           })
         ]
